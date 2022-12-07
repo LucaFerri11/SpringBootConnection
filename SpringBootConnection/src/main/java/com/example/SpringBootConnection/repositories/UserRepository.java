9cf4082ebb4b0bb1e18391718ec796da666627bd
LucaFerri11 <luca.ferri@solera.com>
2022-12-07 10:41:22 +0100

    pt 2

--- SpringBootConnection/src/main/java/com/example/SpringBootConnection/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.SpringBootConnection.repositories;
-
-import com.example.SpringBootConnection.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
